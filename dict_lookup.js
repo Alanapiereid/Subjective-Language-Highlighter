dc6c8c02a622e8df55c9bbf2f9c784e6ae7f6f5c
Alan Reid <56002246+Alanapiereid@users.noreply.github.com>
2021-02-04 00:26:39 +0100

    Add files via upload

---
+++ dict_lookup.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    //var m_list = ['that', 'idiot', 'fraud', 'malevolent'];
+    var sent = document.getElementById("sent").value;
+    var text_out = []
+    console.log(sent)
+  
+    document.getElementById("popup").innerHTML = "dummy text"
+      
+    var splitsent = sent.split(" ");
+  
+    console.log(splitsent)
+  
+    for (var i = 0; i < splitsent.length; i++) {
+      if(strong_subjective.includes(splitsent[i].toLowerCase())) {text_out = text_out + splitsent[i] + " "
+    }
+    document.getElementById("printstate").innerHTML = "The following words are considered subjective:"
+    document.getElementById("popup").innerHTML = text_out;
+  }
+  
+  }
